30df4db47b12bafee7ee884a2bc1c2a72bfd40bf
Mayur Pagare <113515678+mayur3724@users.noreply.github.com>
2023-09-08 13:51:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+	let n1=0,n2=1,nextTerm;
+	nextTerm=n1+n2;
+	while(nextTerm<=num){
+		console.log(nextTerm);
+		n1=n2;
+		n2=nextTerm;
+		nextTerm=n1+n2;
+	}
 }
-
 module.exports = fibonacci;
